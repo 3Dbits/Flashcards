c6ea7604c3d40859a4682b73370b3ac563653d83
3Dbits <102523168+3Dbits@users.noreply.github.com>
2022-09-22 09:27:51 +0200

    Add files via upload

---
+++ flashcards/Main.java
@@ -0,0 +1,13 @@
+package flashcards;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DeckLibApp app = new DeckLibApp();
+        Scanner sc = new Scanner(System.in);
+
+        app.flashMenu();
+
+    }
+}
